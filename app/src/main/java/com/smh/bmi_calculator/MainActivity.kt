3a24c3449700f99f3107c031edc55ed7e406bf17
MinhyeokSun <s.minyeok@gmail.com>
2021-12-09 22:01:27 +0900

    2nd Commit

--- app/src/main/java/com/smh/bmi_calculator/MainActivity.kt
+++ app/src/main/java/com/smh/bmi_calculator/MainActivity.kt
@@ -2,10 +2,28 @@ package com.smh.bmi_calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val heightEditText: EditText = findViewById(R.id.heightEditText) // 첫번째 선언 방법.
+        val weightEditText = findViewById<EditText>(R.id.weightEditText) // 두번째 선언 방법.
+        val resultButton = findViewById<Button>(R.id.resultButton)
+
+        resultButton.setOnClickListener {
+            Log.d("MainActivity","ResultButton이 클릭되었습니다.")
+
+            val height: Int = heightEditText.text.toString().toInt()
+            val weight: Int = weightEditText.text.toString().toInt()
+
+            Log.d("MainActivity","height $height, weight$weight")
+            //앱 죽음. 빈 값을 넣으면 Int로 변환되지 않음. 조건을 달아줘야겠음.
+        }
     }
 }
